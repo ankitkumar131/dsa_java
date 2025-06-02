66b39ea1e6965222279bde8c40064e1c4aa8b3ca
ankitkumar131 <ak3057955@gmail.com>
2025-06-02 13:11:43 +0530

    loops basics 02

---
+++ javaBasics.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class javaBasics{
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+        int income = input.nextInt();
+
+        int tax;
+        
+        if(income <500000){
+            tax = 0;
+           
+        }
+
+        else if(income>500000 && income<1000000){
+            tax = (int) (income*0.2); 
+            
+        }
+
+        else{
+            tax = (int)(income * 0.3);
+            
+        }
+        System.out.println("your tax is " + tax);
+
+    }
+}
